0fe056dc67faed7ff87e755093f232794f439e56
Bryan.Jaramillo <bryanjaramillobaldeon@gmail.com>
2023-05-21 04:32:58 +0100

    add skull king privacy policy page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import PrivacyPolicyFree from '@/views/PrivacyPolicyFree'
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/SkullKingPrivacyPolicy",
+    name: "Skull King Privacy Policy",
+    component: PrivacyPolicyFree
   }
 ];
 
---
+++ src/views/PrivacyPolicyFree.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="display: flex; width: 100%; justify-content: flex-start; align-items: center; flex-direction: column">
+    <h1>Privacy Policy for Skull King Counter</h1>
+    <p>I do not collect any data from you, but google play needs this page anyway, so just enjoy the game</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PrivacyPolicyFree'
+}
+</script>
+
+<style scoped>
+
+</style>
